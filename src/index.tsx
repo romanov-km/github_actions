86d991e9ac4e43f7ad22c6b6f8ce34a42cbced40
Kirill Romanov <workrkm@mail.ru>
2024-07-18 14:39:25 +0300

    fix: Заменить айди яндекс счетчика

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ const domNode = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(domNode);
 root.render(
 	<StrictMode>
-		<Analytic.Provider value={{ yandexId: 96728508, googleId: 'G-ZXWBHB1NR0' }}>
+		<Analytic.Provider value={{ yandexId: 97864625, googleId: 'G-ZXWBHB1NR0' }}>
 			<BrowserRouter
 				basename={process.env.PUBLIC_PATH ? process.env.PUBLIC_PATH : '/'}>
 				<App />
